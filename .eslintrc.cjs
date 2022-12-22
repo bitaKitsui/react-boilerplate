65ec31a774976d08b643dff95199c2faab0810be
bitaKitsui <takaki_kitsui@bita.jp>
2022-12-22 16:22:39 +0900

    feat: add storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,26 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: ["plugin:react/recommended", "standard-with-typescript", "prettier"],
-  overrides: [],
+  extends: ["plugin:react/recommended", "standard-with-typescript", "prettier", "plugin:storybook/recommended"],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
-    project: "./tsconfig.json",
+    project: "./tsconfig.json"
   },
+  overrides: [
+    {
+      files: ['*.stories.@(ts|tsx|js)'],
+      extends: ['plugin:storybook/recommended']
+    }
+  ],
   plugins: ["react"],
   ignorePatterns: ["vite-env.d.ts", "vite.config.ts"],
   rules: {
     "react/react-in-jsx-scope": "off",
-  },
+    "@typescript-eslint/consistent-type-definitions": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/consistent-type-assertions": "off"
+  }
 };
