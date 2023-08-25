4a010c3c6d8824a45db9bc48200821ad90d993fc
bitaKitsui <takaki_kitsui@bita.jp>
2023-08-25 16:21:14 +0900

    chore: setup import order

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,8 @@ module.exports = {
     "extends": [
         "plugin:react/recommended",
         "plugin:react-hooks/recommended",
-        "standard-with-typescript"
+        "standard-with-typescript",
+        "prettier"
     ],
     "overrides": [
     ],
@@ -18,11 +19,29 @@ module.exports = {
     "plugins": [
         "react",
         "react-hooks",
-        "prettier"
+        "import",
+        "unused-imports",
     ],
     "rules": {
         "react/react-in-jsx-scope": "off",
-        "@typescript-eslint/consistent-type-definitions": ["error", "type"]
+        "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+        "@typescript-eslint/no-unused-vars": "off",
+        "import/order": [
+            "error",
+            {
+                groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+                alphabetize: { order: "asc", caseInsensitive: true },
+                pathGroups: [
+                    {
+                        pattern: "react**",
+                        group: "external",
+                        position: "before",
+                    }
+                ],
+                pathGroupsExcludedImportTypes: ["react"]
+            }
+        ],
+        "unused-imports/no-unused-imports": "error",
     },
     "ignorePatterns": ["vite-env.d.ts", "vite.config.ts"],
     "settings": {
