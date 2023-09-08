43dc70ccba58ae893087f140a3c6764dbd8b4628
bitaKitsui <takaki_kitsui@bita.jp>
2023-09-08 14:55:37 +0900

    feat: add useCounter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,36 @@
 import { type FC } from "react";
+import { create } from "zustand";
 import "./App.css";
 
+type UseCounterType = {
+  counter: number;
+  incrementCounter: () => void;
+};
+
+const useCounter = create<UseCounterType>((set) => {
+  return {
+    counter: 0,
+    incrementCounter: () => {
+      set((state) => ({ counter: state.counter + 1 }));
+    },
+  };
+});
+
+const DisplayCounter: FC = () => {
+  const counter = useCounter((state) => state.counter);
+  return <div>Counter: {counter}</div>;
+};
+
+const CounterControl: FC = () => {
+  const incrementCounter = useCounter((state) => state.incrementCounter);
+  return <button onClick={incrementCounter}>Increment</button>;
+};
+
 export const App: FC = () => {
-  return <></>;
+  return (
+    <div>
+      <DisplayCounter />
+      <CounterControl />
+    </div>
+  );
 };
